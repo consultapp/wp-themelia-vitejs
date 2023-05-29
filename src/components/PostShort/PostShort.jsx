e1ed9837f92f376a661a22eaca28ddbf2de20d10
Dmitry Egorov <info@consultapp.ru>
2023-05-29 19:13:15 +0400

    Sidebar

--- src/components/PostShort/PostShort.jsx
+++ src/components/PostShort/PostShort.jsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
 
+function renderMarkdownToHTML(markdown) {
+  // This is ONLY safe because the output HTML
+  // is shown to the same user, and because you
+  // trust this Markdown parser to not have bugs.
+  const renderedHTML = md.render(markdown);
+  return { __html: renderedHTML };
+}
+
 export default function PostShort({ isLoading, post }) {
   if (isLoading) return <Loading />;
   if (!post) return <div>Error???</div>;
 
-  const { id, excerpt, slug, title, link } = post;
+  const { id, excerpt, slug, title } = post;
   const postTitle = title?.rendered || "";
-  const postExcerpt = excerpt.rendered;
+
+  const regex = "</?a(|s+[^>]+)>";
+  console.log(excerpt.rendered.replace(regex, ""));
+
+  const postExcerpt = excerpt.rendered.replace(regex, "");
 
   return (
     <div
@@ -25,7 +37,7 @@ export default function PostShort({ isLoading, post }) {
         </h2>
       </header>
       <div className="entry-summary" itemProp="description">
-        <p>{postExcerpt}</p>
+        <p dangerouslySetInnerHTML={{ __html: postExcerpt }}></p>
         <Link to={`/${slug}`} className="entry-more-link">
           <span>Читать далее</span>
           <span className="screen-reader-text">{postTitle}</span>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,7 +8,13 @@ export default function Sidebar() {
       itemScope="itemscope"
       itemType="http://schema.org/WPSideBar"
     >
-      AsideArea
+      <h3
+        id="sidebar-primary-title"
+        className="screen-reader-text font-headlines"
+      >
+        Sidebar
+      </h3>
+      <div className="sidebar-inner"></div>
     </aside>
   );
 }
