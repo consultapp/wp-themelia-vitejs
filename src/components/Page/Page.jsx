3cf8821e9ea7555356c17459b63c1457cf2d2c89
Dmitry Egorov <info@consultapp.ru>
2023-06-03 13:37:58 +0400

    page

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -4,8 +4,28 @@ export default function Page({ page, isLoading }) {
   if (isLoading) return <Loading />;
   if (!page) return <div>Error of loading</div>;
 
-  const { title } = page;
+  const { id, title, content } = page;
   const renderTitle = title?.rendered;
+  const renderContent = content?.rendered;
 
-  return <div>{renderTitle}</div>;
+  return (
+    <article
+      id={id}
+      className="entry author-consultapp page type-page status-publish"
+      itemScope="itemscope"
+      itemType="http://schema.org/CreativeWork"
+    >
+      <header className="entry-header">
+        <h1 className="entry-title font-headlines" itemProp="headline">
+          {renderTitle}
+        </h1>
+      </header>
+      <div
+        className="entry-content"
+        itemProp="text"
+        dangerouslySetInnerHTML={{ __html: renderContent }}
+      ></div>
+      <footer className="entry-footer small font-secondary"></footer>
+    </article>
+  );
 }
