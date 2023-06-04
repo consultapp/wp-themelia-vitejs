a7c49c212e8e4e58c1d374ccf11077a22e828e31
Dmitry Egorov <info@consultapp.ru>
2023-06-04 23:11:07 +0400

    scroll to the top when  location changed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,16 @@
 import HeaderContainer from "../containers/Header/Header";
 import FooterContainer from "../containers/Footer/Footer";
 import Workarea from "../components/Workarea/Workarea";
+import { useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 
 export default function Layout({ children }) {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <HeaderContainer />
