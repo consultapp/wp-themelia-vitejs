382a92a3d122d3b6887ee2ebb4791b0a68310b48
Dmitry Egorov <info@consultapp.ru>
2023-06-04 22:55:37 +0400

    Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -38,24 +38,6 @@ export default function Menu({ isOpen, toggleButton }) {
             <Link to="/post/simple-bot-for-telegram-php/">Telegram Bots</Link>
           </li>
           <li
-            id="menu-item-1106"
-            className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1106"
-          >
-            <Link to="/post/error/">Post 404</Link>
-          </li>
-          <li
-            id="menu-item-1106"
-            className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1106"
-          >
-            <Link to="/page/error/">Page 404</Link>
-          </li>
-          {/* <li
-        id="menu-item-1105"
-        className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1105"
-      >
-        <Link to="/post/плагины-wp/">WordPress</Link>
-      </li> */}
-          <li
             id="menu-item-657"
             className="menu-item menu-item-type-post_type menu-item-object-post menu-item-657"
           >
